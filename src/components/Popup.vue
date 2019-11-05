cc9820b2273c906b313ebca3c7092796a422e4d3
Pedro Obando <pedroobando@hotmail.com>
2019-11-05 00:23:52 -0400

    transnocho

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,24 +6,20 @@
     <v-card>
       <v-card-title class="headline grey lighten-2">Add new project</v-card-title>
       <v-card-text>
-        <v-form class="px-3">
-          <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="mdi-file-edit"></v-textarea>
-          <v-text-field label="Person" v-model="person" prepend-icon="mdi-file-edit"></v-text-field>
+        <v-form class="px-3" ref="form" v-model="formValid">
+          <v-text-field label="Title" v-model="title" :rules="inputRules" :counter="10" required prepend-icon="mdi-folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" :rules="inputRules" :counter="20" required prepend-icon="mdi-file-edit"></v-textarea>
+          <v-text-field label="Person" v-model="person" :rules="inputRules" prepend-icon="mdi-file-edit"></v-text-field>
           
           <v-menu v-model="menu2" :close-on-content-click="false" max-width="290">
-
             <template v-slot:activator="{ on }">
-              <v-text-field :value="formattedDate" clearable label="Due date" readonly v-on="on">
+              <v-text-field :value="formattedDate" label="Due date" prepend-icon="mdi-calendar" readonly v-on="on">
               </v-text-field>
             </template>
-            <v-date-picker v-model="due" @change="menu2 = false"></v-date-picker>            
+            <v-date-picker v-model="due2" @change="seleccionfecha"></v-date-picker>            
           </v-menu>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="success darken-1" text @click="submit">Add project</v-btn>
-          </v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success darken-1" depressed @click="submit" :disabled="!formValid">Add project</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -31,7 +27,7 @@
 </template>
 
 <script>
-import format from 'date-fns/format';
+import formatfns from 'date-fns/format';
 
 export default {
 
@@ -42,23 +38,43 @@ export default {
     content: '',
     person: '',
     due: new Date(),
+    due2: new Date().toISOString().substr(0, 7),
+    formValid: false,
+    inputRules: [
+      v => !!v || 'Data is required.',
+      v => v.length >= 3 || 'Minimum length is 3 characters'
+    ]
   }),
   methods: {
     submit() {
-      console.log(this.title, this.content);
-      this.dialog = false;
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.content, this.person, this.due);
+        this.dialog = false;
+      } else {
+        this.formValid = false;
+      }
+    },
+    seleccionfecha(fecha) {
+      // console.log(fecha);
+      // console.log(fecha.substr(0,4)+ '-'+fecha.substr(5,2)+'-'+fecha.substr(8,2));
+      this.due = new Date(fecha.substr(0,4), fecha.substr(5,2), fecha.substr(8,2));
+      // console.log(this.due);
+      // this.formattedDate();
+      this.menu2 = false;
     }
+
+        
   },
   computed: {
+    formatPicker() {
+      const retFormat = formatfns(this.due,'yyyy-MM-dd');
+      return retFormat;
+    },
     formattedDate() {
-      // const due = new Date();
       try {
-        const valuex = this.due.toISOString().substr(0, 10)
-        console.log(`formatted: ${valuex}`);
-        const retFormat = format(valuex, 'do MMM yyyy');
-        console.log(retFormat);
-        return retFormat;
+        return this.due ? formatfns(this.due, 'do MMM yyyy') : ''
       } catch(error) {
+        console.log(error);
         return '999999';
       }
     },
